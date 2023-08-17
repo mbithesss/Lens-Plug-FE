03e6cbb5438ba7ce086275bd02eeb22a1045fca8
Job Karani <jobkarani084j@gmail.com>
2023-08-18 01:04:56 +0300

    feat: carousel

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    let slideIndex = 0;
+    showSlides();
 
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+      let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 4000);
+    }
+  }
 
 }
